503ca63013f1d212f845cf436f2395fad4b6e826
Esraa Said <esraa195182@feng.bu.edu.eg>
2026-02-06 01:24:11 +0200

    cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const morgan = require("morgan");
 const sequelize = require("./config/connectDB");
 const globalErrorHandler = require("./middlewares/global-error-middleware");
 const CustomError = require("./utils/custom-error");
 const authRouter = require("./routes/auth-routes");
 const taskRouter = require("./routes/task-routes");
-
 
 // sequelize.authenticate().then(() => {
 //    console.log('Connection has been established successfully.');
@@ -16,11 +16,13 @@ const taskRouter = require("./routes/task-routes");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use('/auth', authRouter);
-app.use('/task', taskRouter);
+app.use("/auth", authRouter);
+app.use("/task", taskRouter);
 
 app.use((req, res, next) => {
   next(new CustomError(`Invalid url`, 404));
